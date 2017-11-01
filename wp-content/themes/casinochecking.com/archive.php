07e900cc538bb32ccc2755914b130dc3a8746dcf
Mads Lundholm <madslundholm@Madss-iMac.local>
2017-11-01 17:36:54 +0100

    Changes from laptop

--- wp-content/themes/casinochecking.com/archive.php
+++ wp-content/themes/casinochecking.com/archive.php
@@ -1,3 +1,4 @@
+Mads Lundholm, [Nov 1, 2017, 5:23:02 PM]:
 <?php
 /**
  * The template for displaying archive pages
@@ -8,44 +9,108 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
+<section class="bg-img"
+style="background: url('http://casinochecking.com/wp-content/uploads/2017/10/casino-bg.jpg') no-repeat center center fixed;">
+<div class="container padding-top">
+	<div class="row welcome-row">
+		<div class="small-12 columns fade-in-slow">
+			<h1>Best casino bonuses.</h1>
+			<h1>Largest profit.</h1>
+		</div>
+	</div>
+	<div class="row slide-up">
+		<div class="small-12 medium-4 bg-sidebar columns border-radius-left wrapper-casino-comparison">
+			<div class="border-radius-left">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
+				$our_score = get_field_object('our_score');
+				$user_votes = get_field_object('user_votes');
 				?>
-			</header><!-- .page-header -->
+				<p>Our score</p>
+				<form class="our-score">
+					<?php foreach ($our_score["choices"] as $k => $v): ?>
+						<div class="our-score-val">
+							<input type="radio" name="radio" value="2">
+							<label for="radio2">
+								<span><span>
+									<div class="value"><?php echo $v; ?></div>
+								</label>
+							</div>
+						<?php endforeach; ?>
+					</form>
+					<?php
+					var_dump($user_votes);
+					?>
+				</div>
+			</div>
+			<div class="small-12 medium-8 bg-gray columns border-radius-right wrapper-casino-comparison">
+				<div class="container">
+					<div class="row">
+						<div class="small-12 columns">
+							<h4 class="headline-casino-h4">Top-rated online casinos</h4>
+							<?php
+							if (have_posts()) : ?>
+							<div class="small-12 columns">
+								<?php while (have_posts()) : the_post(); ?>
+									<div class="row casino-wrapper">
+										<?php
+										$image = get_field('image');
+										$imageSize = 'thumbnail';
+										$deepUrl = get_field('deep_url');
+										$ourScore = get_field('our_score');
+										$userVotes = get_field('user_votes');
+										$signUpBonus = get_field('signup_bonus');
+										$minimumDeposit = get_field('minimum_deposit'); ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+										<div class="small-6 columns border-right my-align-center">
+											<div style="width: 100%;">
+												<div class="image section-padding">
+													<a href="<?php echo the_permalink(); ?>">
+														<?php echo wp_get_attachment_image($image['id'], $imageSize); ?>
+													</a>
+												</div>
+												<div class="user-rating section-padding-bottom">
+													<div class="row">
+														<div class="small-6 columns">
+															<p>User rating</p>
+															<?php include(locate_template('template-parts/parts/user-rating.php')); ?>
+														</div>
+														<div class="small-6 columns">
+															<p>score</p>
+															<h2><?php echo $ourScore; ?></h2>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="small-6 columns my-align-center">
+											<div style="width: 100%;">
+												<div class="image-ratings section-padding">
+													<div class="row">
+														<div class="small-6 border-right">
+															<p>Bonus</p>
+															<h4><?php echo $signUpBonus; ?>$</h4>
+														</div>
+														<div class="small-6">
+															<p>Deposit</p>
+															<h4><?php echo $minimumDeposit; ?>$</h4>
+														</div>
+													</div>
+												</div>
+												<div class="bonus-info button-padding-bottom">
+													<a href="<?php echo $deepUrl; ?>" class="button">Get
+													Bonus</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <?php
-get_sidebar();
 get_footer();
--- wp-content/themes/casinochecking.com/functions.php
+++ wp-content/themes/casinochecking.com/functions.php
@@ -9,127 +9,152 @@ ini_set('display_errors', 1);
  * @package checkmate
  */
 
-/* Adding new tab */ 
-if( function_exists('acf_add_options_page') ) {
+add_action("pre_get_posts", "custom_front_page");
+function custom_front_page($wp_query)
+{
+    //Ensure this filter isn't applied to the admin area
+    if (is_admin()) {
+        return;
+    }
 
-	acf_add_options_page(array(
-		'page_title' 	=> 'Theme General Settings',
-		'menu_title'	=> 'Theme Settings',
-		'menu_slug' 	=> 'theme-general-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
-	));
+    if ($wp_query->get('page_id') == get_option('page_on_front')):
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
-		'menu_title'	=> 'Footer',
-		'parent_slug'	=> 'theme-general-settings',
-	));
+        $wp_query->set('post_type', 'casino');
+        $wp_query->set('page_id', ''); //Empty
+
+        //Set properties that describe the page to reflect that
+        //we aren't really displaying a static page
+        $wp_query->is_page = 0;
+        $wp_query->is_singular = 0;
+        $wp_query->is_post_type_archive = 1;
+        $wp_query->is_archive = 1;
+    endif;
 }
 
-if ( ! function_exists( 'checkmate_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function checkmate_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on checkmate, use a find and replace
-		 * to change 'checkmate' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'checkmate', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+if (function_exists('acf_add_options_page')) {
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+    acf_add_options_page(array(
+        'page_title' => 'Theme General Settings',
+        'menu_title' => 'Theme Settings',
+        'menu_slug' => 'theme-general-settings',
+        'capability' => 'edit_posts',
+        'redirect' => false
+    ));
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+    acf_add_options_sub_page(array(
+        'page_title' => 'Theme Footer Settings',
+        'menu_title' => 'Footer',
+        'parent_slug' => 'theme-general-settings',
+    ));
+}
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'desktop-menu' => esc_html__( 'Desktop - Header', 'checkmate' ),
-			'mobile-header' => esc_html__( 'Mobile Header', 'checkmate' ),
-		));
+if (!function_exists('checkmate_setup')) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function checkmate_setup()
+    {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on checkmate, use a find and replace
+         * to change 'checkmate' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('checkmate', get_template_directory() . '/languages');
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'checkmate_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(array(
+            'desktop-menu' => esc_html__('Desktop - Header', 'checkmate'),
+            'mobile-header' => esc_html__('Mobile Header', 'checkmate'),
+        ));
+
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support('html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
+
+        // Set up the WordPress core custom background feature.
+        add_theme_support('custom-background', apply_filters('checkmate_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        )));
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
+
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support('custom-logo', array(
+            'height' => 250,
+            'width' => 250,
+            'flex-width' => true,
+            'flex-height' => true,
+        ));
+    }
 endif;
-add_action( 'after_setup_theme', 'checkmate_setup' );
+add_action('after_setup_theme', 'checkmate_setup');
 
-function create_post_type() {
-	register_post_type( 'casino',
-		array(
-			'labels' => array(
-				'name' => __( 'Casino' ),
-				'singular_name' => __( 'casinos' )
-			),
-			'public' => true,
-			'has_archive' => true,
-			'dash-icon' => 'dashicons-chart-area',
-		)
-	);
+function create_post_type()
+{
+    register_post_type('casino',
+        array(
+            'labels' => array(
+                'name' => __('Casino'),
+                'singular_name' => __('casinos')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'dash-icon' => 'dashicons-chart-area',
+        )
+    );
 
-	register_post_type( 'blog',
-		array(
-			'labels' => array(
-				'name' => __( 'Blog' ),
-				'singular_name' => __( 'blogs' )
-			),
-			'public' => true,
-			'has_archive' => true,
-			'dash-icon' => 'dashicons-chart-area',
-		)
-	);
+    register_post_type('blog',
+        array(
+            'labels' => array(
+                'name' => __('Blog'),
+                'singular_name' => __('blogs')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'dash-icon' => 'dashicons-chart-area',
+        )
+    );
 }
-add_action( 'init', 'create_post_type' );
+
+add_action('init', 'create_post_type');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -138,43 +163,50 @@ add_action( 'init', 'create_post_type' );
  *
  * @global int $content_width
  */
-function checkmate_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'checkmate_content_width', 640 );
+function checkmate_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('checkmate_content_width', 640);
 }
-add_action( 'after_setup_theme', 'checkmate_content_width', 0 );
+
+add_action('after_setup_theme', 'checkmate_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function checkmate_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'checkmate' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'checkmate' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+function checkmate_widgets_init()
+{
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar', 'checkmate'),
+        'id' => 'sidebar-1',
+        'description' => esc_html__('Add widgets here.', 'checkmate'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
 }
-add_action( 'widgets_init', 'checkmate_widgets_init' );
 
-function accept_svg($mimes) {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+add_action('widgets_init', 'checkmate_widgets_init');
+
+function accept_svg($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
 }
+
 add_filter('upload_mimes', 'accept_svg');
 
 /**
  * Google fonts
  */
-function google_fonts() {
-	$query_args = array(
-		'family' => 'Lato|Roboto'
-	);
-	wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
+function google_fonts()
+{
+    $query_args = array(
+        'family' => 'Lato|Roboto'
+    );
+    wp_register_style('google_fonts', add_query_arg($query_args, "//fonts.googleapis.com/css"), array(), null);
 }
 
 add_action('wp_enqueue_scripts', 'google_fonts');
@@ -182,25 +214,28 @@ add_action('wp_enqueue_scripts', 'google_fonts');
 /**
  * Enqueue scripts and styles.
  */
-function checkmate_scripts() {
-	wp_enqueue_style( 'checkmate-style', get_stylesheet_uri() );
+function checkmate_scripts()
+{
+    wp_enqueue_style('checkmate-style', get_stylesheet_uri());
 
-	// jquery
-	wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+    // jquery
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
 
-	// foundation scripts
-	wp_enqueue_script( 'checkmate-what-input', get_template_directory_uri() . '/bower_components/what-input/dist/what-input.js', array(), '20151215', true );
-	wp_enqueue_script( 'checkmate-foundation', get_template_directory_uri() . '/bower_components/foundation-sites/dist/js/foundation.js', array(), '20151215', true );
+    // foundation scripts
+    wp_enqueue_script('checkmate-what-input', get_template_directory_uri() . '/bower_components/what-input/dist/what-input.js', array(), '20151215', true);
+    wp_enqueue_script('checkmate-foundation', get_template_directory_uri() . '/bower_components/foundation-sites/dist/js/foundation.js', array(), '20151215', true);
 
-	// theme scripts
-	wp_enqueue_script( 'checkmate-scripts', get_template_directory_uri() . '/js/scripts.js', array(), '20151215', true );
+    // theme scripts
+    wp_enqueue_script('checkmate-fontawesome', 'https://use.fontawesome.com/ccfb9ddc23.js', array(), '20151215', false);
+    wp_enqueue_script('checkmate-scripts', get_template_directory_uri() . '/js/scripts.js', array(), '20151215', true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'checkmate_scripts' );
+
+add_action('wp_enqueue_scripts', 'checkmate_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -225,21 +260,24 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if (defined('JETPACK__VERSION')) {
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
 /*Admin*/
-function login_logo() { 
-	?> 
-	<style type="text/css"> 
-	body.login div#login h1 a {
-		background-image: url(<?php echo get_template_directory_uri() . '/screenshot.png'; ?>);  //Add your own logo image in this url 
-		padding-bottom: 30px; 
-		background-size: 100%;
-		width: 320px;
-		height: 240px;
-	} 
-</style>
-<?php 
-} add_action( 'login_enqueue_scripts', 'login_logo' );
+function login_logo()
+{
+    ?>
+    <style type="text/css">
+        body.login div#login h1 a {
+            background-image: url(<?php echo get_template_directory_uri() . '/screenshot.png'; ?>);
+        / / Add your own logo image in this url padding-bottom: 30 px;
+            background-size: 100%;
+            width: 320px;
+            height: 240px;
+        }
+    </style>
+    <?php
+}
+
+add_action('login_enqueue_scripts', 'login_logo');
