0ab608bc1fc12697bb9bf6f971bb203a52aaac4e
Mads Lundholm <madslundholm@Madss-iMac.local>
2017-11-03 18:39:14 +0100

    Filtering

--- wp-content/themes/casinochecking.com/archive-casino.php
+++ wp-content/themes/casinochecking.com/archive-casino.php
@@ -19,27 +19,38 @@ get_header(); ?>
 			<div class="small-12 medium-4 bg-sidebar columns border-radius-left wrapper-casino-comparison">
 				<div class="border-radius-left">
 					<!-- Our score -->
-					<?php $our_score = get_field_object('our_score'); ?>
-					<label class="<?php echo $our_score['name'];?>" data-filter-type="<?php echo $our_score['name'];?>" data-min="<?php echo $our_score['min'];?>" data-max="<?php echo $our_score['max'];?>" for="<?php echo $our_score['name'];?>"><?php echo $our_score['label'];?></label>
-					<input type="text" id="<?php echo $our_score['name'];?>" readonly style="border:0; color:#f6931f; font-weight:bold;">
-					<div id="slider-range-our-score"></div>
+					<div class="filter-inner-wrapper">
+						<?php $our_score = get_field_object('our_score'); ?>
+						<p><label class="<?php echo $our_score['name'];?>" data-filter-type="<?php echo $our_score['name'];?>" data-min="<?php echo $our_score['min'];?>" data-max="<?php echo $our_score['max'];?>" for="<?php echo $our_score['name'];?>"><?php echo $our_score['label'];?></label></p>
+						<div id="slider-range-our-score"></div>
+						<input type="text" id="<?php echo $our_score['name'];?>" readonly>
+					</div>
 					<!-- User votes -->
-					<?php $user_votes = get_field_object('user_votes'); ?>
-					<label class="<?php echo $user_votes['name'];?>" data-filter-type="<?php echo $user_votes['name'];?>" data-min="<?php echo $user_votes['min'];?>" data-max="<?php echo $user_votes['max'];?>" for="<?php echo $user_votes['name'];?>"><?php echo $user_votes['label'];?></label>
-					<input type="text" id="<?php echo $user_votes['name'];?>" readonly style="border:0; color:#f6931f; font-weight:bold;">
-					<div id="slider-range-user-votes"></div>
+					<div class="filter-inner-wrapper">
+						<?php $user_votes = get_field_object('user_votes'); ?>
+						<p><label class="<?php echo $user_votes['name'];?>" data-filter-type="<?php echo $user_votes['name'];?>" data-min="<?php echo $user_votes['min'];?>" data-max="<?php echo $user_votes['max'];?>" for="<?php echo $user_votes['name'];?>"><?php echo $user_votes['label'];?></label></p>
+						
+						<div id="slider-range-user-votes"></div>
+						<input type="text" id="<?php echo $user_votes['name'];?>" readonly>
+					</div>
 					<!-- Minimum deposit -->
-					<?php $min_deposit = get_field_object('minimum_deposit'); ?>
-					<label class="<?php echo $min_deposit['name'];?>" data-filter-type="<?php echo $min_deposit['name'];?>" for="<?php echo $min_deposit['name'];?>"><?php echo $min_deposit['label'];?>
-					</label>
-					<input type="text" id="<?php echo $min_deposit['name'];?>" readonly style="border:0; color:#f6931f; font-weight:bold;">
-					<div id="slider-range-min-deposit"></div>
+					<div class="filter-inner-wrapper">
+						<?php $min_deposit = get_field_object('minimum_deposit'); ?>
+						<p><label class="<?php echo $min_deposit['name'];?>" data-filter-type="<?php echo $min_deposit['name'];?>" for="<?php echo $min_deposit['name'];?>"><?php echo $min_deposit['label'];?>
+						</label></p>
+						
+						<div id="slider-range-min-deposit"></div>
+						<input type="text" id="<?php echo $min_deposit['name'];?>" readonly>
+					</div>
 					<!-- Signup bonus -->
-					<?php $signup_bonus = get_field_object('signup_bonus'); ?>
-					<label class="<?php echo $signup_bonus['name'];?>" data-filter-type="<?php echo $signup_bonus['name'];?>" for="<?php echo $signup_bonus['name'];?>"><?php echo $signup_bonus['label'];?>
-					</label>
-					<input type="text" id="<?php echo $signup_bonus['name'];?>" readonly style="border:0; color:#f6931f; font-weight:bold;">
-					<div id="slider-range-signup-bonus"></div>
+					<div class="filter-inner-wrapper no-border">
+						<?php $signup_bonus = get_field_object('signup_bonus'); ?>
+						<p><label class="<?php echo $signup_bonus['name'];?>" data-filter-type="<?php echo $signup_bonus['name'];?>" for="<?php echo $signup_bonus['name'];?>"><?php echo $signup_bonus['label'];?>
+						</label></p>
+						
+						<div id="slider-range-signup-bonus"></div>
+						<input type="text" id="<?php echo $signup_bonus['name'];?>" readonly>
+					</div>
 					<div class="search-ajax">
 						<a class="button">Search</a>
 					</div>
@@ -47,46 +58,36 @@ get_header(); ?>
 			</div>
 			<div class="small-12 medium-8 bg-gray columns border-radius-right wrapper-casino-comparison">
 				<div class="container">
-					<div class="row">
-						<div class="small-6">
+					<div class="row align-middle padding-bottom">
+						<div class="small-8 columns">
 							<h4 class="headline-casino-h4">Top-rated online casinos</h4>
 						</div>
-						<div class="small-6">
+						<div class="small-4 columns align-right">
 							<div class="filter">
-								<div class="row">
-									<div class="small-3">
-										<p id="filter-headline">Filter:</p>
-									</div>
-									<div class="small-3">
-										<p>Min deposit</p>
-									</div>
-									<div class="small-3">
-										<p>Bonus</p>
-									</div>
-									<div class="small-3">
-										<p>Score</p>
-									</div>
-								</div>
+								<p>Deposit</p>
+								<p>Bonus</p>
+								<p>Score</p>
 							</div>
 						</div>
-						<div class="small-12 columns load-casino">
-							<?php
-							if (have_posts()) : ?>
-							<div class="small-12 columns ">
-								<div class="load-wrapper">
-									<div class="loader">
-									</div></div>
-								</div>
-								<?php while (have_posts()) : the_post(); ?>
-									<?php include(locate_template('template-parts/parts/casino-teaser.php')); ?>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
 					</div>
+					<div class="small-12 columns load-casino">
+						<?php
+						if (have_posts()) : ?>
+						<div class="small-12 columns ">
+							<div class="load-wrapper">
+								<div class="loader">
+								</div></div>
+							</div>
+							<?php while (have_posts()) : the_post(); ?>
+								<?php include(locate_template('template-parts/parts/casino-teaser.php')); ?>
+							<?php endwhile; ?>
+						</div>
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 </section>
 <?php
 get_footer();
