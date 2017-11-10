65f4be0269cda76f197bc6624189e0c43b2ca426
Mads Lundholm <madslundholm@Madss-iMac.local>
2017-11-11 00:57:44 +0100

    Blog created

--- wp-content/themes/casinochecking.com/archive-casino.php
+++ wp-content/themes/casinochecking.com/archive-casino.php
@@ -7,7 +7,7 @@
  * @package checkmate
  */
 get_header(); ?>
-<section class="bg-img" style="background: url('http://casinochecking.com/wp-content/uploads/2017/10/casino-bg.jpg') no-repeat center center fixed;">
+<section class="bg-img" style="background: url('http://casinochecking.com/wp-content/uploads/2017/10/casinocheckingcom-2000x1329.jpg') no-repeat center center fixed;">
 	<div class="container padding-top">
 		<div class="row welcome-row">
 			<div class="small-12 columns fade-in-slow">
