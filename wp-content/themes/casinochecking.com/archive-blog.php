ea7e8bf357fb7126604b16beb96c19ade43755a6
Mads Lundholm <madslundholm@Madss-iMac.local>
2017-11-15 22:42:38 +0100

    Https images fixed

--- wp-content/themes/casinochecking.com/archive-blog.php
+++ wp-content/themes/casinochecking.com/archive-blog.php
@@ -8,7 +8,7 @@
  */
 get_header(); ?>
 <?php $activateOverlay = get_field('activate_overlay_blog', 'options'); ?>
-<section class="bg-img <?php if($activateOverlay): echo 'overlay'; endif;?>" style="background: url('http://casinochecking.com/wp-content/uploads/2017/10/casinocheckingcom-2000x1329.jpg') no-repeat center center fixed;">
+<section class="bg-img <?php if($activateOverlay): echo 'overlay'; endif;?>" style="background: url('https://casinochecking.com/wp-content/uploads/2017/10/casinocheckingcom-2000x1329.jpg') no-repeat center center fixed;">
 	<div class="row welcome-row z-index">
 		<div class="small-12 columns fade-in-slow z-index">
 			<h1><?php the_field('heading_first_line_blog', 'options'); ?></h1>
