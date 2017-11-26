68d05460ee8fed4c04ea2e1ddb1e86b4d18001b8
Mads Lundholm <madslundholm@Madss-iMac.local>
2017-11-26 17:45:30 +0100

    ..

--- wp-content/themes/casinochecking.com/template-parts/parts/user-rating.php
+++ wp-content/themes/casinochecking.com/template-parts/parts/user-rating.php
@@ -1,8 +1,8 @@
 <?php
 for ($x = 1; $x <= $userVotes; $x++) { ?>
-    <img class="star" src="<?php echo get_site_url(null, '/wp-content/uploads/2017/10/gold-star.png', 'https'); ?>">
+    <img src="<?php echo get_site_url(null, '/wp-content/uploads/2017/10/gold-star.png', 'https'); ?>">
 <?php } ?>
 <?php $remainingStars = 5 - $userVotes;
 for ($x = 1; $x <= $remainingStars; $x++) { ?>
-    <img class="star" src="<?php echo get_site_url(null, '/wp-content/uploads/2017/10/grey-star.png', 'https'); ?>">
+    <img src="<?php echo get_site_url(null, '/wp-content/uploads/2017/10/grey-star.png', 'https'); ?>">
 <?php } ?>
