c9d3853cae4528246bcd99b6425f0c43dd705b75
Mads Lundholm <madslundholm@Madss-iMac.local>
2017-11-30 09:39:50 +0100

    Sorting fucking works,bby

--- wp-content/themes/casinochecking.com/functions.php
+++ wp-content/themes/casinochecking.com/functions.php
@@ -8,7 +8,26 @@ ini_set('display_errors', 1);
  *
  * @package checkmate
  */
+add_action("pre_get_posts", "custom_front_page");
+function custom_front_page($wp_query){
+    //Ensure this filter isn't applied to the admin area
+    if(is_admin()) {
+        return;
+    }
 
+    if($wp_query->get('page_id') == get_option('page_on_front')):
+
+        $wp_query->set('post_type', 'casino');
+        $wp_query->set('page_id', ''); 
+        
+        $wp_query->is_page = 0;
+        $wp_query->is_singular = 0;
+        $wp_query->is_post_type_archive = 1;
+        $wp_query->is_archive = 1;
+
+    endif;
+
+}
 add_action('wp_ajax_nopriv_filter_casino', 'filter_casino');
 add_action('wp_ajax_filter_casino', 'filter_casino');
 
