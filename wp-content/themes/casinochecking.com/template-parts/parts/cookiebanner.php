562768b47ab96c3d50ee665660ea902989403bf3
Mads Lundholm <madslundholm@Madss-iMac.local>
2018-03-10 16:30:25 +0100

    Single casino

--- wp-content/themes/casinochecking.com/template-parts/parts/cookiebanner.php
+++ wp-content/themes/casinochecking.com/template-parts/parts/cookiebanner.php
@@ -1,11 +1,8 @@
 <div id="cookiebar">
     <div class="container">
         <div class="row align-middle">
-            <div class="small-9 medium-4 medium-offset-4 columns button-wrap" id="cookie-text">
-                <p><?php echo _e('Psst. We use cookies to improve your experience.', 'checkmate'); ?>
-                </p>
-            </div>
-            <div class="small-3 medium-4 columns text-left">
+            <div class="small-9 medium-6 medium-offset-3 columns button-wrap" id="cookie-text">
+                <p><?php echo _e('Psst. We use cookies to improve your experience.', 'checkmate'); ?></p>
                 <a href="#" id="accept-cookies">OK</a>
             </div>
         </div>
