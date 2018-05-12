af06019b3eaab3f497749650927aeec492e91cc5
Mads Lundholm <madslundholm@Madss-MacBook-Pro-4.local>
2018-05-12 21:11:51 +0200

    New styles

--- wp-content/themes/casinochecking.com/js/scripts.js
+++ wp-content/themes/casinochecking.com/js/scripts.js
@@ -165,6 +165,10 @@ function setCurrencyRate(data) {
             if ($('.post-type-archive-casino').length) {
                 loadCasinoParams(currencyRate);
             }
+            else {
+                fixed_sidebar('#sidebar', '#sticky-item', 'header', '#sticky-item-wrapper');
+                fade_in_elements();
+            }
         },
         error: function (errorThrown) {
             console.log(errorThrown);
